d0b530cc6691606dcf72ee1b8d25eeacda788c54
mehmetfaahem <mehmetfaahem@gmail.com>
2024-11-16 14:53:26 +0600

    build error fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,10 @@
 module.exports = {
   extends: 'next/core-web-vitals',
-  plugins: ['@marblism'],
+  plugins: [],
   rules: {
     'react-hooks/exhaustive-deps': 'off',
     'react/jsx-key': 'warn',
     '@next/next/no-img-element': 'off',
-    '@marblism/no-react-query-in-functions': 'error',
     'react/no-unescaped-entities': 'off',
   },
 }
