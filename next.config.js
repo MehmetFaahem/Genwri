7ea198259e6d5b88ffab6ceb88db8a1eb4dc9eed
mehmetfaahem <mehmetfaahem@gmail.com>
2024-11-17 15:48:24 +0600

    runtime increased

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -7,8 +6,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: false,
-
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  // Add custom config for longer timeouts
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiTimeout: 60000, // 60 seconds
+  },
 }
 
 export default config
-
